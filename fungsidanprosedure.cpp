c78e06753ded5b09cfed6c1ea871322b052d5ac3
EA Nugroho <69884456+JourneyEAN@users.noreply.github.com>
2023-01-16 13:33:42 +0700

    Create fungsidanprosedure.cpp

---
+++ fungsidanprosedure.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+//fungsi prosedur 
+void fullname() {
+    cout<<"Halo Nama Saya Eko Ari Nugroho"<<endl;
+    cout<<"Saya Mahasiswa Universitas Indonesia Mandiri"<<endl;
+}
+
+int main()
+{
+  fullname();
+  return 0;
+}
